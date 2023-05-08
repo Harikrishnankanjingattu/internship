b02105890fcc8f4161283ce4913cb334c9de60e9
Harikrishnankanjingattu <132329896+Harikrishnankanjingattu@users.noreply.github.com>
2023-05-08 15:57:01 +0530

    Create exp3.py

---
+++ exp3.py
@@ -0,0 +1,15 @@
+ a=str(input("enter string \t"))
+if a=="a":
+    print("vowel")
+elif a=="e":
+    print("vowel")
+elif a=="i":
+    print("vowel")
+elif a=="o":
+    print("vowel")
+
+elif a=="u":
+    print("vowel")
+else:
+    print("not vowel")
+  
