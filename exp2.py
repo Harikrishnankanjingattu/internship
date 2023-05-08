d970e32c11e699c43feb0b77f561ebf0007811a9
Harikrishnankanjingattu <132329896+Harikrishnankanjingattu@users.noreply.github.com>
2023-05-08 15:06:45 +0530

    Create exp2.py

---
+++ exp2.py
@@ -0,0 +1,13 @@
+a=int(input("enter first number:-"))
+b=int(input("enter second  number"))
+c=int(input("enter third   number"))
+if a<b:
+   
+    if c<b:
+        print("bigger is b")
+    else:
+        print("bigg is c ")
+elif c<a:
+   print("bigg is a")
+else: 
+    print("bigger is c")
