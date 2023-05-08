d183917bc7c106c4b120429371a02049254a316f
Harikrishnankanjingattu <132329896+Harikrishnankanjingattu@users.noreply.github.com>
2023-05-08 11:44:20 +0530

    Create exp5.py

---
+++ exp5.py
@@ -0,0 +1,6 @@
+print("biodata of student ")
+name = "harikrishnan k"
+age=17
+college="GPTC CHELAKKARA"
+pla="Arangottukara"
+print("self intro- \n" ,"my name is", name  ,   " \n",  age ," years  old \n","iam form",college,"\n","myplace is",pla)
